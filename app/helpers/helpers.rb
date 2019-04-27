ddc124b83cd6eedff8fc0b9283976034c589507a
Heather Kirk <hskirk@gmail.com>
2019-04-27 00:21:35 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,11 @@
 class Helpers
   
   def self.current_user(session)
-    binding.pry
     @user = User.find(session[:user_id])
   end 
   
   def self.is_logged_in?(session)
+    binding.pry 
     !!session(:user_id)
   end 
 end
