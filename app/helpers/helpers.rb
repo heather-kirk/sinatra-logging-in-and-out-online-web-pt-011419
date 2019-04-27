0b790a21ac0bb79303a4cd398011bcfbfb668349
Heather Kirk <hskirk@gmail.com>
2019-04-27 00:05:17 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,10 @@
 class Helpers
   
   def current_user(session)
-    
+    session[:user_id] = user.id 
   end 
   
   def is_logged_in?(session)
-    !!@user.id(session)
+    !!session(:user_id)
   end 
 end
